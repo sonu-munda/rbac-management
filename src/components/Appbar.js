18e5c5eb2788de719c76b1a3c153b7e1dfce16c9
Sonu Munda <itssonuinfo@gmail.com>
2024-11-28 07:03:27 +0530

    logout button added

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState, useLayoutEffect, useEffect } from 'react'
 import '../styles/appbar.css'
+import '../styles/tooltips.css'
 
 export default function Appbar() {
+
+    let [displayingIcon, setDisplayingIcon] = useState(false);
+    const [width, height] = useWindowSize();
+
+    const onMenuTap = () => {
+        setDisplayingIcon(!displayingIcon)
+    }
+
+    useEffect(() => {
+        if (displayingIcon && width <= 767) {
+            setDisplayingIcon(false)
+        } else if (!displayingIcon && width > 767) {
+            setDisplayingIcon(true)
+        }
+    }, [width])
+
     return (
+
         <header className="appbar">
+            <img src={displayingIcon ? 'close.png' : 'menu.png'} className='menu-icon' onClick={onMenuTap} />
             <h1>RBAC Management System</h1>
+            {displayingIcon && <div className='tooltip'>
+                <div className='logged-in-user'>
+                    <img src='profile-user.png' />
+                    <span className='logout-button'>LOGOUT</span>
+                </div>
+                <span class="tooltiptext">sonu@gmail.com</span>
+            </div>}
         </header>
     )
+}
+
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+        function updateSize() {
+            setSize([window.innerWidth, window.innerHeight]);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
 }
--- src/components/HomeButtons.js
+++ src/components/HomeButtons.js
@@ -5,11 +5,11 @@ export default function HomeButtons() {
     return (
         <div className='home-button-container'>
             <Link to='/home/users' href='' className='home-button' >
-                <img src='../../users.png' />
+                <img src='users.png' />
                 <h4>USERS</h4>
             </Link>
             <Link to='/home/users' href='' className='home-button' >
-                <img src='../../roles.png' />
+                <img src='roles.png' />
                 <h4>ROLES</h4>
             </Link>
         </div>
