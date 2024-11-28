c958da96065c9c14ed405729e1eb4617be471b31
Sonu Munda <itssonuinfo@gmail.com>
2024-11-28 07:37:57 +0530

    post edit delete icon added

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,38 +3,58 @@ import '../styles/feed.css'
 
 export default function Feed() {
 
-
-
+    const feeds = [
+        {
+            id: 1,
+            userId: 3,
+            author: 'Sonu Munda',
+            imageUrl: 'https://via.placeholder.com/150',
+            title: 'Post Title 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer acligula eget sapien aliquet scelerisque.'
+        },
+        {
+            id: 2,
+            userId: 3,
+            author: 'Sonu Munda',
+            imageUrl: 'https://via.placeholder.com/150',
+            title: 'Post Title 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer acligula eget sapien aliquet scelerisque.'
+        },
+        {
+            id: 3,
+            userId: 4,
+            author: 'Anonymous',
+            imageUrl: 'https://via.placeholder.com/150',
+            title: 'Post Title 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer acligula eget sapien aliquet scelerisque.'
+        },
+        {
+            id: 4,
+            userId: 4,
+            author: 'Anonymous',
+            imageUrl: 'https://via.placeholder.com/150',
+            title: 'Post Title 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer acligula eget sapien aliquet scelerisque.'
+        },
+    ]
     return (
         <div className="feed-container">
             <header className="header">
                 <h1>Post Feed</h1>
             </header>
             <main className="feed">
-                <div className="post">
-                    <img src="https://via.placeholder.com/150" alt="Post Image" className="post-image" />
+                {feeds.map(post => <div className="post">
+                    <img src={post.imageUrl} alt="Post Image" className="post-image" />
                     <div className="post-content">
-                        <h2 className="post-title">Post Title 1</h2>
-                        <p className="post-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac
-                            ligula eget sapien aliquet scelerisque.</p>
+                        <h2 className="post-title">{post.title}</h2>
+                        <p className="post-description">{post.description}</p>
+                        <span className='post-author'>~{post.author}</span>
                     </div>
-                </div>
-                <div className="post">
-                    <img src="https://via.placeholder.com/150" alt="Post Image" className="post-image" />
-                    <div className="post-content">
-                        <h2 className="post-title">Post Title 2</h2>
-                        <p className="post-description">Aliquam vestibulum, eros id aliquam rhoncus, orci ligula egestas lectus,
-                            at sodales urna justo ut risus.</p>
+                    <div className='post-icons-container'>
+                        <img src='edit.png' alt="Post Image" className="post-image" />
+                        <img src='bin.png' alt="Post Image" className="post-image" />
                     </div>
-                </div>
-                <div className="post">
-                    <img src="https://via.placeholder.com/150" alt="Post Image" className="post-image" />
-                    <div className="post-content">
-                        <h2 className="post-title">Post Title 3</h2>
-                        <p className="post-description">Curabitur sed arcu sit amet massa gravida sodales. Donec vehicula metus
-                            nec ante commodo, id tincidunt sem porttitor.</p>
-                    </div>
-                </div>
+                </div>)}
             </main>
         </div>
     )
